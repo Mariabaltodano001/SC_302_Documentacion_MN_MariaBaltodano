c3da0693222f736139412c86ec86179411a9b999
Mariabaltodano001 <157537097+Mariabaltodano001@users.noreply.github.com>
2024-01-23 15:28:53 -0600

    Update Miarchivo.java

--- miarchivo/src/miarchivo/Miarchivo.java
+++ miarchivo/src/miarchivo/Miarchivo.java
@@ -15,7 +15,7 @@ public class Miarchivo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo"); 
+        System.out.println("hola mundo_editado"); 
 
         // cambios en el archivo :)
     }
